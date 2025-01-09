2ab575511bbdc3686e8dfe428669f32f7a824229
zootya <levelkardoszoltannak@gmail.com>
2025-01-09 22:57:22 +0100

    valuta valtasa

--- frontend/js/valto.js
+++ frontend/js/valto.js
@@ -1,23 +1,47 @@
 //const myUrl = "http://127.0.0.1";
 const myUrl = "http://azenhazam.mywire.org";
 
-function getArfolyam(){
+var id = [];
+var penznem = [];
+var arfolyam = [];
+var valutanevek = [];
+
+
+function szamol(){
+    const mirol = document.querySelector("#inputSelect").value;
+    const mire = document.querySelector("#outputSelect").value;
+    const mennyit = document.querySelector("#mennyi").value;
+    const euroid = penznem.indexOf("EUR");
+    
+    // 1 USA dollár = EUR/USD*HUF = 401.6 forint
+    keresem = arfolyam[euroid] / arfolyam[id.indexOf(parseInt(mirol))] * arfolyam[id.indexOf(parseInt(mire))];
+    visszefelekeresem = arfolyam[euroid] / arfolyam[id.indexOf(parseInt(mire))] * arfolyam[id.indexOf(parseInt(mirol))];
+    
+    document.querySelector(".valtas1sor").innerHTML = mennyit + " " + penznem[id.indexOf(parseInt(mirol))] + " - " + valutanevek[id.indexOf(parseInt(mirol))] + " =";
+
+    document.querySelector(".valtas2sor").innerHTML = mennyit * keresem + " " + penznem[id.indexOf(parseInt(mire))] + " - " + valutanevek[id.indexOf(parseInt(mire))];
+
+    document.querySelector(".valtas3sor").innerHTML = "1 " + penznem[id.indexOf(parseInt(mirol))] + " = " + keresem + " " + penznem[id.indexOf(parseInt(mire))] + "<br>" + "1 " + penznem[id.indexOf(parseInt(mire))] + " = " + visszefelekeresem + " " + penznem[id.indexOf(parseInt(mirol))];
+    
+}
+
+
+function getValutaadatok(){
+    document.querySelector("#inputSelect").innerHTML = "";
+    document.querySelector("#outputSelect").innerHTML = "";
+
     fetch(`${myUrl}:8800/restadat`).then(res=>res.json()).then(result=>{
         result.forEach(item => {
-            document.querySelector("#fetchResult").innerHTML +=
-            `
-            <div class = "col-12 col-md-6 col-lg-3">
-                <img class = "card-img-top" src = "${item.kepUrl}" alt = "">
-                    <div class = "card-body">
-                        <h4 class = card-title>${item.arfolyam}</h4>
-                        <p class = "card-text">${item.id}</p>
-                        <p class = "card-text">${item.valutanevek} - ${item.penznem}</p>
-                    </div>
-                </div> 
-            </div>
-            `;
+            item.penznem == "EUR" ? document.querySelector("#inputSelect").innerHTML +=`<option value = "${item.id}" selected>${item.valutanevek}</option>` : document.querySelector("#inputSelect").innerHTML +=`<option value = "${item.id}">${item.valutanevek}</option>`;
+            
+            item.penznem == "HUF" ? document.querySelector("#outputSelect").innerHTML +=`<option value = "${item.id}" selected>${item.valutanevek}</option>` : document.querySelector("#outputSelect").innerHTML +=`<option value = "${item.id}">${item.valutanevek}</option>`;
+
+            id.push(item.id);
+            penznem.push(item.penznem);
+            arfolyam.push(item.arfolyam);
+            valutanevek.push(item.valutanevek);
         });
     })
 }
 
-getArfolyam()
+getValutaadatok()
