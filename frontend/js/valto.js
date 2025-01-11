3186b8197ddf3557a4b1253107d37a9425bbe0a1
zootya <levelkardoszoltannak@gmail.com>
2025-01-11 11:40:05 +0100

    valtasi hibakezeles

--- frontend/js/valto.js
+++ frontend/js/valto.js
@@ -10,21 +10,31 @@ var valutanevek = [];
 function szamol(){
     const mirol = document.querySelector("#inputSelect").value;
     const mire = document.querySelector("#outputSelect").value;
-    const mennyit = parseInt(document.querySelector("#mennyi").value);
+    var mennyit = parseFloat(document.querySelector("#mennyi").value);
     const euroid = penznem.indexOf("EUR");
     const forintid = penznem.indexOf("HUF");
-    
+
+    var hiba = "";
+    isNaN(mennyit) ? hiba = "Nem értelmezhető a mennyiség!" : "";
+    if (mennyit < 0) {
+        hiba = `Biztos ezt akartad? Én nem ${mennyit} számolok, hanem ${Math.abs(mennyit)}!`;
+        mennyit = Math.abs(mennyit);
+    }
+
     // 1 USA dollár = EUR/USD*HUF = 401.6 forint
     keresem = arfolyam[euroid] / arfolyam[id.indexOf(parseInt(mirol))] * arfolyam[id.indexOf(parseInt(mire))];
     visszefelekeresem = arfolyam[euroid] / arfolyam[id.indexOf(parseInt(mire))] * arfolyam[id.indexOf(parseInt(mirol))];
 
     odaforint = parseFloat((arfolyam[euroid] / arfolyam[id.indexOf(parseInt(mirol))] * arfolyam[forintid] * mennyit).toFixed(4));
     
+    document.querySelector(".valtas0sor").innerHTML = hiba;
     document.querySelector(".valtas1sor").innerHTML = mennyit.toLocaleString('hu-HU') + " " + penznem[id.indexOf(parseInt(mirol))] + " - " + valutanevek[id.indexOf(parseInt(mirol))] + " =";
     document.querySelector(".valtas2sor").innerHTML = parseFloat((mennyit * keresem).toFixed(4)).toLocaleString('hu-HU') + " " + penznem[id.indexOf(parseInt(mire))] + " - " + valutanevek[id.indexOf(parseInt(mire))];
     document.querySelector(".valtas3sor").innerHTML = "1 " + penznem[id.indexOf(parseInt(mirol))] + " = " + keresem.toLocaleString('hu-HU') + " " + penznem[id.indexOf(parseInt(mire))] + "<br>" + "1 " + penznem[id.indexOf(parseInt(mire))] + " = " + visszefelekeresem.toLocaleString('hu-HU') + " " + penznem[id.indexOf(parseInt(mirol))];
     
     odaforint * 0.009 < 34900 ? illetek = odaforint * 0.009 : illetek = 34900;
+    isNaN(odaforint) ? illetek = 0 : illetek = illetek;
+ 
     document.querySelector(".valtas4sor").innerHTML = `
         <b style="color:red"> ${illetek.toLocaleString('hu-HU')} HUF</b> a kezelési költség!<br>
     `;
